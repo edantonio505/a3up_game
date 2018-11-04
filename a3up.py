5729d694a77a8b86ff6529eec9be885517e14d12
Edgar Velazquez <edantonio505@gmail.com>
2018-11-04 02:50:53 -0500

    creating a rectangle that jumps

--- a3up.py
+++ a3up.py
@@ -1,44 +1,78 @@
-#!/anaconda3/bin/python3
 import pygame
+import sys
 
-
-
-
-
-SCREEN_W = 800
-SCREEN_H = 600
-POS_X = SCREEN_W // 2
-POS_Y = SCREEN_H // 2
-WIDTH = 50
-HEIGHT = 70
-
-
-
-BLACK = (0, 0, 0)
 
 
 def main():
-	
-
-	
 	pygame.init()
+	pygame.display.set_caption("First actual game")
 
-	pygame.display.set_caption("First Python Game")
-	screen = pygame.display.set_mode((SCREEN_W, SCREEN_H))
+	# WINDOW SIZE
+	HEIGHT = 600 
+	WIDTH = 800
 
-	screen.fill((0,0,0))
+	# COLOR
+	BLACK = 0, 0, 0
+	RED = 255, 0, 0
+
+	# Characters dimensions
+	RECT_WIDTH = 60
+	RECT_HEIGHT = 80
+	x = (WIDTH // 2) - (RECT_HEIGHT//2)
+	y = HEIGHT - RECT_HEIGHT - 2
+
+
 	
+	
+	# Velocity
+	VEL = 5
+
+
+
+	is_jump = False
+	jump_count = 10
+
+
+	screen = pygame.display.set_mode((WIDTH, HEIGHT))
 	running = True
 
-	while running:
 
+
+
+	while running:
 		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
+			if event.type == pygame.QUIT: 
 				running = False
-	screen.fill(BLACK)	
-	pygame.draw.rect(screen, (255, 0, 0), (POS_X, POS_Y, WIDTH, HEIGHT))
+				sys.exit()
+
+		keys = pygame.key.get_pressed()
+
+		if keys[pygame.K_LEFT] and x > 0:
+			x -= VEL
+		if keys[pygame.K_RIGHT] and x < WIDTH - RECT_WIDTH:
+			x += VEL
+		if not is_jump:
+			if keys[pygame.K_SPACE]:
+				is_jump = True
+		else:
+			if jump_count >= -10:
+				neg = 1
+				if jump_count < 0:
+					neg = -1
+				y -= (jump_count**2) * (0.5) * neg
+				jump_count -= 1
+			else:
+				is_jump = False
+				jump_count = 10
+		screen.fill(BLACK)
+		#SURFACE, COLOR,  (POSITION X, POSITION Y, HEIGHT, WIDTH)
+		pygame.draw.rect(screen, RED, (x, y, RECT_WIDTH, RECT_HEIGHT))
+		pygame.display.update()
+			
+
+
 	
-	pygame.display.flip()
+
 
 if __name__ == "__main__":
 	main()
